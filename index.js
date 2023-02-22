9b8853ffe5e38f1c5960f2c1baa679564ec689ef
Alex Di Guida <alexandre.diguida@gmail.com>
2023-02-22 11:05:41 +0900

    Add cors middleware

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const path = require('path')
 const port = process.env.PORT || 3001
 
+/**
+ * Middlewares
+ */
 app.use(express.static('public'))
+app.use(cors())
 
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, 'public') })
 })
